632a67b9de126d46f20b497b6b27c1d8a613ef27
Rafał Wolak <r.wolak@design4.pro>
2022-03-04 14:16:38 +0100

    fix(test): fix memoize for test

--- libs/angular-jss/src/lib/hooks/use-memo.ts
+++ libs/angular-jss/src/lib/hooks/use-memo.ts
@@ -1,9 +1,33 @@
-import memoizeOne from 'memoize-one';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function weakMemoize(fn: any, resolver?: any) {
+  const cache = new WeakMap();
+  // instead of returning the function right away, store it in a variable...
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const memoized = function (...args: any) {
+    // eslint-disable-next-line prefer-spread
+    const key = resolver ? resolver.apply(null, args) : args[0];
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      // eslint-disable-next-line prefer-spread
+      const result = fn.apply(null, args);
+
+      cache.set(key, result);
+
+      return result;
+    }
+  };
+  // add a method to it to get the cache
+  memoized.getCache = () => cache;
+  // now return the function
+  return memoized;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useMemo<T = any>(create: () => T, ...args: any): T {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const memoized: any = memoizeOne(create);
+  const memoized: any = weakMemoize(create);
 
   return memoized(args);
 }
